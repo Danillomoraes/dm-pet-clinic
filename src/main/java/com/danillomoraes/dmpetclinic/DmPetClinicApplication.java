d1cced497a3e0729659ce1c26373bf9bbda93649
Danillo Moraes <danillo.moraes@outlook.com>
2019-08-26 20:49:08 -0300

    Init Project pet-clinic

---
+++ src/main/java/com/danillomoraes/dmpetclinic/DmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.danillomoraes.dmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmPetClinicApplication.class, args);
+	}
+
+}
